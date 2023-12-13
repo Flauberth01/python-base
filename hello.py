874b8a0edbdc0e5f8048636e684f2576f8db8614
Flauberth01 <flauberth12@gmail.com>
2023-12-13 13:30:01 -0500

    feat: implementar versão 0.1.3 do hello world multi linguas

--- hello.py
+++ hello.py
@@ -11,32 +11,44 @@ Tenha a variável LANG devidamente configurada ex:
 
     export LANG=pt_BR
 
+Ou informe através do CLI argument `--lang`
+
+Ou o usuário terá que digitar.
+
 Execução:
 
     python3 hello.py
     ou
     ./hello.py
 """
-__version__ = "0.0.1"
+__version__ = "0.1.3"
 __author__ = "Flauberth Brito"
 __license__ = "Unlicense"
 
 import os
 import sys
 
-arguments = {
-    "lang": None,
-    "count": None,
-}
+arguments = {"lang": None, "count": 1,}
+
 for arg in sys.argv[1:]:
-    # TODO: 
+    # TODO: tratar ValueError
     key, value = arg.split("=")
     key = key.lstrip("-").strip()
+    value = value.strip()
     if key not in arguments:
         print(f"Invalid Option `{key}`")
-    print(key, value)
+        sys.exit()
+    arguments[key] = value
 
-current_language = os.getenv("LANG", "en_US")[:5]
+current_language = arguments["lang"]
+if current_language is None:
+    # TODO: Usar repetição
+    if "LANG" in os.environ:
+        current_language = os.getenv("LANG")
+    else:
+        current_language = input("Choose a language: ")
+
+current_language = current_language[:5]
 
 msg = {
     "en_US": "Hello, World!",
@@ -46,5 +58,7 @@ msg = {
     "fr_FR": "Bonjour Monde",
 }
     
-print(msg[current_language])
+print(
+    msg[current_language] * int(arguments["count"])
+)
  
