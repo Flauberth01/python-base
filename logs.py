69dd240b4be0f2e4d99a532acee25185d6e3b460
Flauberth01 <flauberth12@gmail.com>
2024-01-12 12:41:47 -0500

    feat: criar script logs.py para o uso de logs

---
+++ logs.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import os
+import logging
+
+log_level = os.getenv("LOG_LEVEL", "WARNING").upper()
+
+# nossa instancia
+log = logging.Logger("flauberth", log_level)
+# level
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+# formatacao
+fmt = logging.Formatter(
+    '%(asctime)s %(name)s %(levelname)s'
+    'l:%(lineno)d f:%(filename)s: %(message)s'
+)
+ch.setFormatter(fmt)
+# destino
+log.addHandler(ch)
+
+log.debug("Mensagem pro dev, qe, sysadmin")
+log.info("Mensagem geral para usuarios")
+log.warning("Aviso que não causa erro")
+log.error("Erro que afeta uma unica execucao")
+log.critical("Erro geral ex: banco de dados sumiu")
+
+print("---")
+
+try:
+    1 / 0
+except ZeroDivisionError as e:
+    log.error("Deu erro %s", str(e))
